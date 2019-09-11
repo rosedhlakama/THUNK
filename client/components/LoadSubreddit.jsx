ee35d8844b4b49ff5aac3fa856059bccc5fa68d2
Student <student@pop-os.localdomain>
2019-09-12 11:55:04 +1200

    on my way

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -1,14 +1,50 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import {fetchPosts} from '../actions'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions'
 
-const LoadSubreddit = ({children, dispatch}) => (
-  <div>
-    <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-      Fetch Posts
+class LoadSubreddit extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      subreddit: ''
+    }
+  }
+  handleChange = (e) => {
+     subreddit = e.target.value
+    }
+  }
+
+  render() {
+    let { children, dispatch } = this.props
+    return (
+      <div>
+        <form>
+          <p>
+            <input type='text' name='subreddit' placeholder='search region' onChange={this.handleChange} />
+          </p>
+
+
+          <button onClick={() => dispatch(fetchPosts('newzealand'))}>
+            Fetch Posts
     </button>
-    {children}
-  </div>
-)
+
+        </form>
+
+        {children}
+      </div>
+    )
+  }
+
+
+
+// const LoadSubreddit = ({children, dispatch}) => (
+
+//   <div>
+//     <button onClick={() => dispatch(fetchPosts('newzealand'))}>
+//       Fetch Posts
+//     </button>
+//     {children}
+//   </div>
+// )
 
 export default connect()(LoadSubreddit)
